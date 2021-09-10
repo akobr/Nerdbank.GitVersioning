19bc531a9c60eaca5e99ccc74c8f8aca2f57590b
Ales Kobr <ales.kobr@bricknode.com>
2021-09-10 10:06:23 +0200

    changes in descriptions of new flags

--- src/NerdBank.GitVersioning/VersionOptions.cs
+++ src/NerdBank.GitVersioning/VersionOptions.cs
@@ -512,10 +512,10 @@ public class VersionOptions : IEquatable<VersionOptions>
     }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the version in current directory structure is hierarchical and calculated independently per each sub-directory.
+        /// Gets or sets a value indicating whether the version in the current directory structure is hierarchical and calculated independently per each sub-directory.
         /// </summary>
         /// <remarks>
-        /// When this is <c>true</c>, the path filters are not applied and rather each sub-folder automatically applies itself as an only filter (included).
+        /// When this is <c>true</c>, the path filters are not applied, and rather each sub-folder automatically applies itself as an only filter (included).
         /// </remarks>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         [System.ComponentModel.DefaultValue(null)]
@@ -527,7 +527,7 @@ public class VersionOptions : IEquatable<VersionOptions>
 
         /// <summary>
         /// Gets or sets a value indicating whether path filters are inherited from parent version configurations.
-        /// This is backward compatibility flag, if you want the behaviour of inheritance on path filters, you must to set the flag to true explicitly.
+        /// This is a backward compatibility flag, if you want the behaviour of inheritance on path filters, you must set the flag to true explicitly.
         /// </summary>
         /// <remarks>
         /// When this is <c>false</c>, the path filters are NOT inherited from parent configurations.
